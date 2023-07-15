b8f48a9a9664b9a690389bd21fb366721d4771c7
Marlin <hello@marlin.red>
2023-07-15 20:12:40 +0800

    change the tab title

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、设计师、细节控、创始人',
+  title: '绘札',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    '欢迎来到「绘札」，我的精神角落，我会在这里倾注热情、分享兴趣和记录灵感。目前，我正在准备 GMAT 考试和留学申请，其中点滴的努力和得失也将被如数呈现。',
   url: new URL(
     process.env.NODE_ENV === 'production'
       ? 'https://cali.so'
